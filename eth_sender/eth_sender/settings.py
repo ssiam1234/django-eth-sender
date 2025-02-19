c7005197438d07ae778c861e84aa8605c988b624
ssiam1234 <sajjadsiam61@gmail.com>
2025-02-20 02:13:11 +0600

    Update settings.py

--- eth_sender/eth_sender/settings.py
+++ eth_sender/eth_sender/settings.py
@@ -17,6 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
